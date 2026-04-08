a933f13765ac033777b6fceb829c9eb6817eb04a
Lívia Oliveira <liviaoliveira.dev@gmail.com>
2026-04-08 00:56:28 -0300

    Delete seeds.sql

--- seeds.sql
+++
@@ -1,6 +0,0 @@
--- INITIAL DATA FOR TESTING | DADOS INICIAIS PARA TESTE
--- Creates the first admin user
--- Cria o primeiro usuário administrador
-
-INSERT INTO funcionarios (nome, email, cargo) 
-VALUES ('Remy', 'remy@ratatouille.com', 'Chef de Cuisine');
